ecad0b2305f60b20b87efae1b0dd32fad706bcb9
cjscrimmy <craigscrimgeour@yahoo.com>
2020-06-16 18:41:12 +0100

    write code to make the book count test pass.

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -2,13 +2,19 @@ import java.util.ArrayList;
 
 public class Library {
     private int capacity;
-
+    private ArrayList<Book> books;
 
     public Library(int capacity){
         this.capacity = capacity;
+        this.books = new ArrayList<Book>();
     }
 
     public int getCapacity(){
         return this.capacity;
     }
+
+    public int bookCount(){
+        return this.books.size();
+    }
+
 }
