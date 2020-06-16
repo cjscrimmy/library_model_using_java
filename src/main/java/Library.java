fae8dca32113ae7349521b8291dd3bd8ccc4b49f
cjscrimmy <craigscrimgeour@yahoo.com>
2020-06-16 18:35:46 +0100

    Write test for getting Library's capacity. Set up Library class and create a method for getting the capacity

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Library {
+    private int capacity;
+
+
+    public Library(int capacity){
+        this.capacity = capacity;
+    }
+
+    public int getCapacity(){
+        return this.capacity;
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+    Library library;
+    Book book1;
+    Book book2;
+
+    @Before
+    public void setUp(){
+        library = new Library(2);
+        book1 = new Book("The Hobbit", "J.R.R. Tolkien", "Fantasy");
+        book2 = new Book("The House at Pooh Corner", "A.A. Milne", "Children");
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(2, library.getCapacity());
+    }
 }
